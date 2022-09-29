c32e8fe16aad86583d390a3bfba763870fff70d5
malalatiseira <tiseiramarialaura@gmail.com>
2022-09-29 20:54:50 -0300

    Updated

---
+++ script.js
@@ -0,0 +1,7 @@
+const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
+if(edadDelUsuario >= 18){
+    alert("Eres mayor de edad, puedes ingresar ✅");
+}else{
+    alert("Eres menor de edad ❌ ");
+    alert("⚠️​ USTED ESTÁ INGRESANDO BAJO SU RESPONABILIDAD​");
+}
